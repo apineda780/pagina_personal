43520aa3f28b0e8cbb7a6765285d02caa07def02
apineda780 <angiepineda780@gmail.com>
2024-10-09 20:59:25 -0500

    Actualización hoja de vida

---
+++ js/main.js
@@ -0,0 +1,8 @@
+console.log("Mi tercer commit");
+
+const btnClick = document.getElementById("btnRegistrar");
+btnClick.addEventListener("click", registrar());
+function registrar () {
+    console.log("click en el boton registrar");
+
+}
